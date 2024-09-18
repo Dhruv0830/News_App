c16690de8bc43e96201a65530a27e8c390d29958
Dhruv Bakshi <dhruvbakshi0803@gmail.com>
2024-09-18 12:04:40 +0530

    Server APIs done and tested on Postman

--- backend/index.js
+++ backend/index.js
@@ -6,12 +6,47 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT;
+const API_KEY = process.env.API_KEY;
 
 app.get("/", (req, res) => {
-  console.log("Server started successfully");
   res.json({
     message: "Server is running",
   });
+});
+
+app.get("/latest", (req, res) => {
+  category = "general";
+  url =
+    "https://gnews.io/api/v4/top-headlines?category=" +
+    category +
+    "&lang=en&country=us&max=10&apikey=" +
+    API_KEY;
+
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      articles = data.articles;
+      res.send(articles);
+    });
+});
+
+app.get("/search", (req, res) => {
+  search_url =
+    "https://gnews.io/api/v4/search?q=" +
+    req.body.search_item +
+    "&lang=en&country=us&max=10&apikey=" +
+    API_KEY;
+
+  fetch(search_url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      articles = data.articles;
+      res.send(articles);
+    });
 });
 
 app.listen(PORT, (err) => {
